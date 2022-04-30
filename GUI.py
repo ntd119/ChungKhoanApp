faa70b50a7d14c80f5ae54f041464d7b57a0f102
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-04-30 19:49:54 +0700

    Add

--- GUI.py
+++ GUI.py
@@ -7,11 +7,11 @@ root.title("MTD Team")
 root.config(width=800, height=400)
 
 data = pandas.read_csv("./file/stock-code.csv")
-stock_code = data.to_dict()["code"]
+stock_code_list = data.to_dict()["code"]
 
-
-
-print(stock_code)
+for i in stock_code_list:
+    stock_code = stock_code_list[i]
+    print(stock_code)
 
 
 
