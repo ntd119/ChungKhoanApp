32cf396b78994a748094b1ffa678f23ca718db79
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-05-01 09:51:13 +0700

    Add

--- GUI.py
+++ GUI.py
@@ -6,10 +6,8 @@ root.title("MTD Team")
 root.config(width=800, height=400)
 stock = Stock()
 
-print (stock.stock_code_list)
 stock.draw_header()
-
-
+stock.draw_body()
 
 
 
--- stock.py
+++ stock.py
@@ -1,5 +1,9 @@
+from tkinter import Entry
+
 import pandas
 from tkinter import *
+
+FONT_HEADER = ("Arial", 10, "bold")
 
 class Stock:
 
@@ -13,20 +17,41 @@ class Stock:
             self.stock_code_list.append({"index": index, "code": row["code"], "min": row["min"], "max": row["max"]})
 
     def draw_header(self):
-        checkbox = Checkbutton()
-        checkbox.grid(column=0, row=0)
-
-        stock_code = Label(text="Mã ck")
+        stock_code = Label(text="Mã ck", font=FONT_HEADER)
         stock_code.grid(column=1, row=0)
 
-        current_value = Label(text="Giá trị hiện tại")
+        current_value = Label(text="Giá trị hiện tại", font=FONT_HEADER)
         current_value.grid(column=2, row=0)
 
-        min_value = Label(text="Ngưỡng min")
+        min_value = Label(text="Ngưỡng min", font=FONT_HEADER)
         min_value.grid(column=3, row=0)
 
-        max_value = Label(text="Ngưỡng max")
+        max_value = Label(text="Ngưỡng max", font=FONT_HEADER)
         max_value.grid(column=4, row=0)
 
-        radio_choose = Label(text="Selected")
+        radio_choose = Label(text="Selected", font=FONT_HEADER)
         radio_choose.grid(column=5, row=0)
+
+    def draw_body(self):
+        for item_dict in self.stock_code_list:
+            row = int(item_dict.get("index")) + 1
+            checkbox = Checkbutton()
+            checkbox.grid(column=0, row=row)
+
+            stock_code = Label(text=item_dict.get("code"))
+            stock_code.grid(column=1, row=row)
+
+            current_value = Label(text="")
+            current_value.grid(column=2, row=row)
+
+            min_value = Entry()
+            min_value.insert(END, item_dict.get("min"))
+            min_value.grid(column=3, row=row)
+
+            max_value = Entry()
+            max_value.insert(END, item_dict.get("max"))
+            max_value.grid(column=4, row=row)
+
+            radio_choose = Label(text="Selected")
+            radio_choose.grid(column=5, row=row)
+
