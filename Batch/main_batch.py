0a49023da5382ad7b881052bef1ac2dbe674073f
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-05-16 21:18:43 +0700

    Add

--- Batch/main_batch.py
+++ Batch/main_batch.py
@@ -1,7 +1,7 @@
 import requests
-import datetime as dt
 import json
 import time
+import datetime as dt
 
 HEADERS = {"X-Requested-With": "XMLHttpRequest",
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -16,52 +16,60 @@ paramters = {
     "languageID": 1
 }
 
-response = requests.get(END_POINT, params=paramters, headers=HEADERS)
-response.raise_for_status()
-data_list = response.json()
 
-data_from_file = {}
-try:
-    with open(FILE_NAME) as stock_file:
-        data_from_file = json.load(stock_file)
-except FileNotFoundError:
-    with open(FILE_NAME, "a") as stock_file:
-        data_from_file = {}
+def update_data():
+    response = requests.get(END_POINT, params=paramters, headers=HEADERS)
+    response.raise_for_status()
+    data_list = response.json()
+    try:
+        with open(FILE_NAME) as stock_file:
+            data_from_file = json.load(stock_file)
+    except FileNotFoundError:
+        with open(FILE_NAME, "a") as stock_file:
+            data_from_file = {}
 
-with open(FILE_NAME, 'w') as stock_file:
-    for data in data_list:
-        stock_code = data["_sc_"]
-        current_price = data["_cp_"]
-        current_time = round(time.time() * 1000)
-        line_price: list
-        try:
-            max_price = max(data_from_file[stock_code]["max_price"], current_price)
-            min_price = min(data_from_file[stock_code]["min_price"], current_price)
-            max_price_time = data_from_file[stock_code]["max_price_time"]
-            if current_price > max_price:
+    with open(FILE_NAME, 'w') as stock_file:
+        for data in data_list:
+            stock_code = data["_sc_"]
+            current_price = data["_cp_"]
+            current_time = round(time.time() * 1000)
+            line_price: list
+            try:
+                max_price = max(data_from_file[stock_code]["max_price"], current_price)
+                min_price = min(data_from_file[stock_code]["min_price"], current_price)
+                max_price_time = data_from_file[stock_code]["max_price_time"]
+                if current_price > max_price:
+                    max_price_time = current_time
+                min_price_time = data_from_file[stock_code]["min_price_time"]
+                if current_price < min_price:
+                    min_price_time = current_time
+                line_price = data_from_file[stock_code]["line_price"]
+                last_item = line_price[-1]
+                last_item_price = last_item["price"]
+                if last_item_price != current_price:
+                    line_price.append({"price":current_price, "time": current_time})
+            except KeyError:
+                max_price = current_price
+                min_price = current_price
                 max_price_time = current_time
-            min_price_time = data_from_file[stock_code]["min_price_time"]
-            if current_price < min_price:
                 min_price_time = current_time
-            line_price = data_from_file[stock_code]["line_price"]
-            last_item = line_price[-1]
-            last_item_price = last_item["price"]
-            if last_item_price != current_price:
-                line_price.append({"price":current_price, "time": current_time})
-        except KeyError:
-            max_price = current_price
-            min_price = current_price
-            max_price_time = current_time
-            min_price_time = current_time
-            line_price = [{"price":current_price, "time": current_time}]
-        stock = {
-            data["_sc_"]: {
-                "max_price": max_price,
-                "max_price_time": max_price_time,
-                "min_price": min_price,
-                "min_price_time": min_price_time,
-                "line_price": line_price,
+                line_price = [{"price":current_price, "time": current_time}]
+            stock = {
+                data["_sc_"]: {
+                    "max_price": max_price,
+                    "max_price_time": max_price_time,
+                    "min_price": min_price,
+                    "min_price_time": min_price_time,
+                    "line_price": line_price,
+                }
             }
-        }
-        data_from_file.update(stock)
-    json.dump(data_from_file, stock_file, indent=4)
+            data_from_file.update(stock)
+        json.dump(data_from_file, stock_file, indent=4)
+
+
+while True:
+    print(f"RUNNING...{dt.datetime.now()}")
+    update_data()
+    time.sleep(5)
+
+
