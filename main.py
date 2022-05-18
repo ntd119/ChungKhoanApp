1295a77223cf29312001a663db3cbf8df16d0feb
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-05-18 19:48:34 +0700

    Add

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ from tkinter import *
 from stock import Stock
 
 root = Tk()
-# w.pack()
-
-# scroll_bar = Scrollbar(root)
-#
-# scroll_bar.pack(side=RIGHT,
-#                 fill=Y)
-
 root.title("MTD Team")
 stock = Stock(root)
 root.config(padx=50, pady=50, width=800, height=400, bg=stock.background_color)
