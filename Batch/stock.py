21452a481f914e5155adc897b4ecae260d93fbd0
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-05-21 19:59:57 +0700

    Add

--- Batch/stock.py
+++ Batch/stock.py
@@ -40,7 +40,6 @@ class Stock:
                 stock_code = data["_sc_"]
                 current_price = int(data["_cp_"])
                 current_time = round(time.time() * 1000)
-                line_price: list
                 try:
                     max_price = max(data_from_file[stock_code]["max_price"], current_price)
                     min_price = min(data_from_file[stock_code]["min_price"], current_price)
@@ -50,30 +49,32 @@ class Stock:
                     min_price_time = data_from_file[stock_code]["min_price_time"]
                     if current_price < min_price:
                         min_price_time = current_time
-                    line_price = data_from_file[stock_code][f"line_price_{day_of_week}"]
-                    last_item = line_price[-1]
-                    last_item_price = last_item["price"]
-                    percent_one = ((current_price - last_item_price) / last_item_price) * 100
-                    if abs(percent_one) > 1:
-                        if last_item_price <= current_price:
-                            if self.up:
-                                line_price[-1] = ({"price": current_price, "time": current_time})
-                            else:
-                                line_price.append({"price": current_price, "time": current_time})
-                            self.up = True
-                        else:
-                            if not self.up:
-                                line_price[-1] = ({"price": current_price, "time": current_time})
-                            else:
-                                line_price.append({"price": current_price, "time": current_time})
-                            self.up = False
+                    line_price_2 = data_from_file[stock_code][f"line_price_2"]
+                    line_price_3 = data_from_file[stock_code][f"line_price_3"]
+                    line_price_4 = data_from_file[stock_code][f"line_price_4"]
+                    line_price_5 = data_from_file[stock_code][f"line_price_5"]
+                    line_price_6 = data_from_file[stock_code][f"line_price_6"]
+                    # if day_of_week == 2:
+                    #     line_price_2 = self.update_line_price(line_price_2, current_price, current_time)
+                    # elif day_of_week == 3:
+                    #     line_price_3 = self.update_line_price(line_price_3, current_price, current_time)
+                    # elif day_of_week == 4:
+                    #     line_price_4 = self.update_line_price(line_price_4, current_price, current_time)
+                    # elif day_of_week == 5:
+                    #     line_price_5 = self.update_line_price(line_price_5, current_price, current_time)
+                    # elif day_of_week == 6:
+                    #     line_price_6 = self.update_line_price(line_price_6, current_price, current_time)
+
                 except KeyError:
                     max_price = current_price
                     min_price = current_price
                     max_price_time = current_time
                     min_price_time = current_time
-                    line_price = [{"price": current_price, "time": current_time},
-                                  {"price": current_price, "time": current_time}]
+                    line_price_2 = []
+                    line_price_3 = []
+                    line_price_4 = []
+                    line_price_5 = []
+                    line_price_6 = []
                 stock = {
                     data["_sc_"]: {
                         "max_price": int(max_price),
@@ -81,7 +82,11 @@ class Stock:
                         "min_price": int(min_price),
                         "min_price_time": min_price_time,
                         "type": data["_in_"],
-                        f"line_price_{day_of_week}": line_price,
+                        "line_price_2": line_price_2,
+                        "line_price_3": line_price_3,
+                        "line_price_4": line_price_4,
+                        "line_price_5": line_price_5,
+                        "line_price_6": line_price_6
                     }
                 }
                 data_from_file.update(stock)
@@ -93,3 +98,25 @@ class Stock:
             os.remove(name)
         for i in range(7, -1, -1):
             os.rename(f'data/stock_T{i}.json', f'data/stock_T{i + 1}.json')
+
+    def update_line_price(self, line_price, current_price, current_time):
+        if len(line_price) < 2:
+            line_price = [{"price": current_price, "time": current_time},
+                          {"price": current_price, "time": current_time}]
+        last_item = line_price[-1]
+        last_item_price = last_item["price"]
+        percent_one = ((current_price - last_item_price) / last_item_price) * 100
+        if abs(percent_one) > 1:
+            if last_item_price <= current_price:
+                if self.up:
+                    line_price[-1] = ({"price": current_price, "time": current_time})
+                else:
+                    line_price.append({"price": current_price, "time": current_time})
+                self.up = True
+            else:
+                if not self.up:
+                    line_price[-1] = ({"price": current_price, "time": current_time})
+                else:
+                    line_price.append({"price": current_price, "time": current_time})
+                self.up = False
+        return line_price
