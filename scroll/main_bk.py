aa36ec056ffa39d2437e89c74da215f0ed6a0848
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-05-22 14:24:01 +0700

    Add

--- scroll/main_bk.py
+++ scroll/main_bk.py
@@ -1,62 +1,105 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.grid_rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
+LABEL_BG = 'light grey'
+ROWS, COLS = 10, 6  # Size of grid.
+ROWS_DISP = 3  # Number of rows to display.
+COLS_DISP = 4  # Number of columns to display.
 
-frame_main = tk.Frame(root, bg="gray")
-frame_main.grid(sticky='news')
 
-label1 = tk.Label(frame_main, text="Label 1", fg="green")
-label1.grid(row=0, column=0, pady=(5, 0), sticky='nw')
+class HoverButton(tk.Button):
+    """ Button that changes color to activebackground when mouse is over it. """
 
-label2 = tk.Label(frame_main, text="Label 2", fg="blue")
-label2.grid(row=1, column=0, pady=(5, 0), sticky='nw')
+    def __init__(self, master, **kw):
+        super().__init__(master=master, **kw)
+        self.default_Background = self.cget('background')
+        self.hover_Background = self.cget('activebackground')
+        self.bind('<Enter>', self.on_enter)
+        self.bind('<Leave>', self.on_leave)
 
-label3 = tk.Label(frame_main, text="Label 3", fg="red")
-label3.grid(row=3, column=0, pady=5, sticky='nw')
+    def on_enter(self, e):
+        self.config(background=self.hover_Background)
 
-# Create a frame for the canvas with non-zero row&column weights
-frame_canvas = tk.Frame(frame_main)
-frame_canvas.grid(row=2, column=0, pady=(5, 0), sticky='nw')
-frame_canvas.grid_rowconfigure(0, weight=1)
-frame_canvas.grid_columnconfigure(0, weight=1)
-# Set grid_propagate to False to allow 5-by-5 buttons resizing later
-frame_canvas.grid_propagate(False)
+    def on_leave(self, e):
+        self.config(background=self.default_Background)
 
-# Add a canvas in that frame
-canvas = tk.Canvas(frame_canvas, bg="yellow")
-canvas.grid(row=0, column=0, sticky="news")
 
-# Link a scrollbar to the canvas
-vsb = tk.Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
-vsb.grid(row=0, column=1, sticky='ns')
-canvas.configure(yscrollcommand=vsb.set)
+class MyApp(tk.Tk):
+    def __init__(self, title='Sample App', *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-# Create a frame to contain the buttons
-frame_buttons = tk.Frame(canvas, bg="blue")
-canvas.create_window((0, 0), window=frame_buttons, anchor='nw')
+        self.title(title)
+        self.configure(background='Gray')
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
-# Add 9-by-5 buttons to the frame
-rows = 9
-columns = 5
-buttons = [[tk.Button() for j in range(columns)] for i in range(rows)]
-for i in range(0, rows):
-    for j in range(0, columns):
-        buttons[i][j] = tk.Button(frame_buttons, text=("%d,%d" % (i+1, j+1)))
-        buttons[i][j].grid(row=i, column=j, sticky='news')
+        master_frame = tk.Frame(self, bg='Light Blue', bd=3, relief=tk.RIDGE)
+        master_frame.grid(sticky=tk.NSEW)
+        master_frame.columnconfigure(0, weight=1)
 
-# Update buttons frames idle tasks to let tkinter calculate buttons sizes
-frame_buttons.update_idletasks()
+        label1 = tk.Label(master_frame, text='Frame1 Contents', bg=LABEL_BG)
+        label1.grid(row=0, column=0, pady=5, sticky=tk.NW)
 
-# Resize the canvas frame to show exactly 5-by-5 buttons and the scrollbar
-first5columns_width = sum([buttons[0][j].winfo_width() for j in range(0, 5)])
-first5rows_height = sum([buttons[i][0].winfo_height() for i in range(0, 5)])
-frame_canvas.config(width=first5columns_width + vsb.winfo_width(),
-                    height=first5rows_height)
+        frame1 = tk.Frame(master_frame, bg='Green', bd=2, relief=tk.FLAT)
+        frame1.grid(row=1, column=0, sticky=tk.NW)
 
-# Set the canvas scrolling region
-canvas.config(scrollregion=canvas.bbox("all"))
+        cb_var1 = tk.IntVar()
+        checkbutton1 = tk.Checkbutton(frame1, text='StartCheckBox', variable=cb_var1)
+        checkbutton1.grid(row=0, column=0, padx=0, pady=0)
 
-# Launch the GUI
-root.mainloop()
+        label2 = tk.Label(master_frame, text='Frame2 Contents', bg=LABEL_BG)
+        label2.grid(row=2, column=0, pady=5, sticky=tk.NW)
+
+        # Create a frame for the canvas and scrollbar(s).
+        frame2 = tk.Frame(master_frame, bg='Red', bd=2, relief=tk.FLAT)
+        frame2.grid(row=3, column=0, sticky=tk.NW)
+
+        # Add a canvas in that frame.
+        canvas = tk.Canvas(frame2, bg='Yellow')
+        canvas.grid(row=0, column=0)
+
+        # Create a vertical scrollbar linked to the canvas.
+        vsbar = tk.Scrollbar(frame2, orient=tk.VERTICAL, command=canvas.yview)
+        vsbar.grid(row=0, column=1, sticky=tk.NS)
+        canvas.configure(yscrollcommand=vsbar.set)
+
+        # Create a horizontal scrollbar linked to the canvas.
+        hsbar = tk.Scrollbar(frame2, orient=tk.HORIZONTAL, command=canvas.xview)
+        hsbar.grid(row=1, column=0, sticky=tk.EW)
+        canvas.configure(xscrollcommand=hsbar.set)
+
+        # Create a frame on the canvas to contain the grid of buttons.
+        buttons_frame = tk.Frame(canvas)
+
+        # Add the buttons to the frame.
+        for i in range(1, ROWS+1):
+            for j in range(1, COLS+1):
+                button = HoverButton(buttons_frame, padx=7, pady=7, relief=tk.RIDGE,
+                                     activebackground= 'orange', text='[%d, %d]' % (i, j))
+                button.grid(row=i, column=j, sticky='news')
+
+        # Create canvas window to hold the buttons_frame.
+        canvas.create_window((0,0), window=buttons_frame, anchor=tk.NW)
+
+        buttons_frame.update_idletasks()  # Needed to make bbox info available.
+        bbox = canvas.bbox(tk.ALL)  # Get bounding box of canvas with Buttons.
+
+        # Define the scrollable region as entire canvas with only the desired
+        # number of rows and columns displayed.
+        w, h = bbox[2]-bbox[1], bbox[3]-bbox[1]
+        dw, dh = int((w/COLS) * COLS_DISP), int((h/ROWS) * ROWS_DISP)
+        canvas.configure(scrollregion=bbox, width=dw, height=dh)
+
+        label3 = tk.Label(master_frame, text='Frame3 Contents', bg=LABEL_BG)
+        label3.grid(row=4, column=0, pady=5, sticky=tk.NW)
+
+        frame3 = tk.Frame(master_frame, bg='Blue', bd=2, relief=tk.FLAT)
+        frame3.grid(row=5, column=0, sticky=tk.NW)
+
+        cb_var2 = tk.IntVar()
+        checkbutton2 = tk.Checkbutton(frame3, text='EndCheckBox', variable=cb_var2)
+        checkbutton2.grid(row=0, column=0, padx=0, pady=0)
+
+
+if __name__ == '__main__':
+    app = MyApp('Scrollable Canvas')
+    app.mainloop()
