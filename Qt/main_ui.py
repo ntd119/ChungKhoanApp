440552fbdd600e7e77a6f7b9e45c6fd83f08034b
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-06-11 18:48:10 +0700

    Add

--- Qt/main_ui.py
+++ Qt/main_ui.py
@@ -1,12 +1,40 @@
+import time
+
 from PyQt5.QtWidgets import QMainWindow
 from qt_form import Ui_MainWindow
+import requests
 
+VIETSTOCK_END_POINT = "https://api.vietstock.vn/finance/sectorInfo_v2"
+HEADERS = {"X-Requested-With": "XMLHttpRequest",
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/54.0.2840.99 Safari/537.36',
+           }
 
 class MainUI:
     def __init__(self):
         self.main_win = QMainWindow()
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self.main_win)
+        self.data_vietstock = None
+        self.run()
+
+    def run(self):
+        self.call_api_vietstock()
+
+    def call_api_vietstock(self):
+        vietstock_prams = {
+            "sectorID": 0,
+            "catID": 0,
+            "capitalID": 0,
+            "languageID": 1
+        }
+        try:
+            response = requests.get(VIETSTOCK_END_POINT, params=vietstock_prams, headers=HEADERS)
+            self.data_vietstock = response.json()
+        except:
+            print("Call vietstock error")
+            time.sleep(5)
+            self.call_api_vietstock()
 
     def show(self):
         self.main_win.show()
