56d1b256a8fba290e5dcb10d0cf6e6c30ec71d77
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-06-12 16:51:30 +0700

    Add

--- HoiPhaDaoCK_v2/run.py
+++ HoiPhaDaoCK_v2/run.py
@@ -1,9 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QTimer
 from main_ui import MainUI
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_win = MainUI()
     main_win.show()
+    timer = QTimer()
+    timer.timeout.connect(main_win.update_table)
+    timer.setInterval(10000)  # 10000ms = 10s
+    timer.start()
     sys.exit(app.exec())
