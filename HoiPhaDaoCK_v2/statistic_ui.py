ae3eb85bf1afdefbf97da6ba88f325b1c38bda1d
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-06-18 14:29:31 +0700

    Add

--- HoiPhaDaoCK_v2/statistic_ui.py
+++ HoiPhaDaoCK_v2/statistic_ui.py
@@ -15,7 +15,23 @@ import unidecode
 class StatisticUI:
     def __init__(self):
         self.window = QMainWindow()
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self.window)
+        self.uic = Ui_Dialog()
+        self.uic.setupUi(self.window)
         self.window.show()
+        self.show_table()
+
+    def show_table(self):
+        nhom_nganh = []
+        for nganh in NHOM_CO_PHIEU:
+            if nganh == FILE_DA_MUA or nganh == FILE_VN30 or nganh == FILE_VN100  or nganh == FILE_TAT_CA:
+                continue
+            nhom_nganh.append(NHOM_CO_PHIEU[nganh]["name"])
+        self.uic.tableWidget.setRowCount(len(nhom_nganh))
+        self.uic.tableWidget.setColumnCount(5)
+        _translate = QtCore.QCoreApplication.translate
+        for row_index, ten_nganh in enumerate(nhom_nganh):
+            item = QtWidgets.QTableWidgetItem()
+            self.uic.tableWidget.setVerticalHeaderItem(row_index, item)
+            item.setText(_translate("MainWindow", ten_nganh))
+
 
