1b0d07b501682236773267ab07b5dd748dfba995
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-06-18 18:47:48 +0700

    Add

--- HoiPhaDaoCK_v2/statistic_ui.py
+++ HoiPhaDaoCK_v2/statistic_ui.py
@@ -13,18 +13,19 @@ class StatisticUI:
         self.uic = Ui_Dialog()
         self.uic.setupUi(self.window)
         self.window.show()
-        self.data_max_min = self.call_api_max_min()
         self.data_vietstock = self.call_api_vietstock()
+        self.data_max_min_0 = self.call_api_max_min(0)
+        self.data_max_min_1 = self.call_api_max_min(1)
         self.show_table()
 
-    def call_api_max_min(self):
+    def call_api_max_min(self, index_file):
         try:
-            response = requests.get(MAX_MIN_END_POINT, headers=HEADERS)
+            response = requests.get(f"https://topchonlua.com/batch/data/stock_T{index_file}.json", headers=HEADERS)
             return response.json()
         except:
             print("Max min api error")
             time.sleep(5)
-            self.call_api_max_min()
+            self.call_api_max_min(index_file)
 
     def call_api_vietstock(self):
         vietstock_prams = {
@@ -44,7 +45,7 @@ class StatisticUI:
     def show_table(self):
         nhom_nganh = []
         for nganh in NHOM_CO_PHIEU:
-            if nganh == FILE_DA_MUA or nganh == FILE_VN30 or nganh == FILE_VN100  or nganh == FILE_TAT_CA:
+            if nganh == FILE_DA_MUA or nganh == FILE_VN30 or nganh == FILE_VN100 or nganh == FILE_TAT_CA:
                 continue
             dict_item = {
                 "name": NHOM_CO_PHIEU[nganh]["name"],
@@ -56,10 +57,10 @@ class StatisticUI:
         _translate = QtCore.QCoreApplication.translate
 
         # set tên cột
-        for i in range(1):
+        for i in range(2):
             item = QtWidgets.QTableWidgetItem()
             self.uic.tableWidget.setHorizontalHeaderItem(i, item)
-            item.setText(_translate("MainWindow", "Giá hiện tại\nso với thứ 2\n0"))
+            item.setText(_translate("MainWindow", f"Giá hiện tại\nso với thứ 2\n{i}"))
 
         # set tên dòng
         for row_index, ten_nganh in enumerate(nhom_nganh):
@@ -67,32 +68,36 @@ class StatisticUI:
             self.uic.tableWidget.setVerticalHeaderItem(row_index, item)
             item.setText(_translate("MainWindow", ten_nganh["name"]))
 
-        for row_index, nganh in enumerate(nhom_nganh):
-            file_name = nganh["file"]
-            percent = self.tinh_phan_tram_thay_doi(file_name)
-            item = QtWidgets.QTableWidgetItem()
-            self.uic.tableWidget.setItem(row_index, 0, item)
-            item.setText(_translate("MainWindow", self.format_2_decimal(percent) + "%"))
-            if percent < 0:
-                self.uic.tableWidget.item(row_index, 0).setBackground(
-                    QtGui.QColor(BACKGROUND_LO))
-            else:
-                self.uic.tableWidget.item(row_index, 0).setBackground(
-                    QtGui.QColor(BACKGROUND_LAI))
-
+        for colum_index in range(2):
+            for row_index, nganh in enumerate(nhom_nganh):
+                file_name = nganh["file"]
+                percent = self.tinh_phan_tram_thay_doi(file_name, colum_index)
+                item = QtWidgets.QTableWidgetItem()
+                self.uic.tableWidget.setItem(row_index, colum_index, item)
+                item.setText(_translate("MainWindow", self.format_2_decimal(percent) + "%"))
+                if percent < 0:
+                    self.uic.tableWidget.item(row_index, colum_index).setBackground(
+                        QtGui.QColor(BACKGROUND_LO))
+                else:
+                    self.uic.tableWidget.item(row_index, colum_index).setBackground(
+                        QtGui.QColor(BACKGROUND_LAI))
 
     def format_2_decimal(self, value):
         return "{:.2f}".format(value)
 
-    def tinh_phan_tram_thay_doi(self, file_name):
+    def tinh_phan_tram_thay_doi(self, file_name, index_file):
         _translate = QtCore.QCoreApplication.translate
         sum_head_price = 0
         sum_tail_price = 0
+
         with open(f"data/{file_name}", "r") as file_data:
             data_from_file = json.load(file_data)
             for row_index, stock_code in enumerate(data_from_file):
                 try:
-                    max_min_dict = self.data_max_min[stock_code]
+                    if index_file == 0:
+                        max_min_dict = self.data_max_min_0[stock_code]
+                    elif index_file == 1:
+                        max_min_dict = self.data_max_min_1[stock_code]
                 except KeyError:
                     continue
                 sum_head_price += max_min_dict["head_price"]
